3baf5ce78bc18e0d7b263aff0d5c088877c40f22
Bryan De La Cruz <bdelacruza@pucp.edu.pe>
2020-07-15 01:25:58 -0500

    juego culminado

--- app.js
+++ app.js
@@ -9,38 +9,96 @@ GAME RULES:
 
 */
 
-var scores, roundScore,activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
-scores=[0,0];
-roundScore=0;
-activePlayer=1;
+init();
 
-document.querySelector('.dice').style.display='none';
-
-document.getElementById('score-0').textContent='0';
-document.getElementById('score-1').textContent='0';
-document.getElementById('current-0').textContent='0';
-document.getElementById('current-1').textContent='0';
-
-document.querySelector('.btn-roll').addEventListener('click',function(){
-    
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  if (gamePlaying) {
     //obtiene un número entre 1 y 6
-    var dice=Math.floor(Math.random()*6)+1;
-    
+    var dice = Math.floor(Math.random() * 6) + 1;
+
     // muestra la imagen del dado dependiendo del número
-    var diceDOM=document.querySelector('.dice');
-    diceDOM.style.display='block';
-    diceDOM.src='dice-'+dice+'.png';
+    var diceDOM = document.querySelector(".dice");
+    diceDOM.style.display = "block";
+    diceDOM.src = "dice-" + dice + ".png";
 
     //Se valida si se obtiene 1
-    if(dice !==1){
-        //Agregar al marcador
-        roundScore +=dice;
-        // document.querySelector('#current-'+activePlayer).textContent=roundScore;
-        document.getElementById('current-'+activePlayer).textContent=roundScore;
+    if (dice !== 1) {
+      //Agregar al marcador
+      roundScore += dice;
+      // document.querySelector('#current-'+activePlayer).textContent=roundScore;
+      document.getElementById(
+        "current-" + activePlayer
+      ).textContent = roundScore;
     } else {
-
+      nextPlayer();
     }
+  }
+});
 
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  if (gamePlaying) {
+    scores[activePlayer] += roundScore;
 
-})
+    document.querySelector("#score-" + activePlayer).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 10) {
+      document.querySelector("#name-" + activePlayer).textContent = "¡GANADOR!";
+      document.querySelector(".dice").style.display = "none";
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.remove("active");
+      gamePlaying = false;
+    } else {
+      nextPlayer();
+    }
+  }
+});
+
+function nextPlayer() {
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+
+  document.getElementById("current-0").textContent = 0;
+  document.getElementById("current-1").textContent = 0;
+
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+  // document.querySelector('.player-0-panel').classList.remove('active');
+  // document.querySelector('.player-1-panel').classList.add('active');
+
+  document.querySelector(".dice").style.display = "none";
+}
+
+document.querySelector(".btn-new").addEventListener("click", init);
+
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+  gamePlaying = true;
+
+  document.querySelector(".dice").style.display = "none";
+
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  document.getElementById("name-0").textContent = "Jugador 1";
+  document.getElementById("name-1").textContent = "Jugador 2";
+
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+
+  document.querySelector(".player-0-panel").classList.add("active");
+}
